30c2a3007d005f55e18cc79ca85a423b79b7e6a0
Parthiv <parthiv@gallery.local>
2026-01-13 21:50:52 +0700

    Chore: Add click logging for debug

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -96,6 +96,7 @@ const Gallery = () => {
     };
 
     const handleImageClick = (image) => {
+        console.log("Clicked Image:", image, "Unlocked:", isUnlocked);
         setSelectedImage(image);
     };
 
